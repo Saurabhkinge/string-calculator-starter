18d377067c224692cc3d3209d04370c4064555a3
Saurabhkinge <92664690+Saurabhkinge@users.noreply.github.com>
2021-10-17 18:00:52 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -4,17 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringCalculatorShould {
+import org.junit.jupiter.api.BeforeEach;
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	class StringCalculatorShould {
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+		private StringCalculator stringCalculator;
+		
+		@BeforeEach
+	    public void init()
+		{
+			stringCalculator = new StringCalculator();
+		}
+		
+		@Test
+	    public void empty_string_should_return_0() 
+		{
+	        assertEquals(0, stringCalculator.add(""));
+	    }
 }
